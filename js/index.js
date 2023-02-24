31dc2cf35fe18249005f8376850c30ea39d68245
lesson of 9 <komiljonovhumoyun982@gmail.com>
2023-02-24 18:35:29 +0500

    js

--- js/index.js
+++ js/index.js
@@ -1,124 +1,23 @@
-// let age=Number(prompt("how old are you?"));
-// let b=+prompt("boyingiz?")
+const showForm = document.getElementById("form");
+const showInput = document.getElementById("input");
+const showP = document.getElementById("p");
 
-// console.log(age,b)
+showInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  if (e.target.value) {
+    showP.textContent = "";
+    showInput.style.borderBottom = "1px solid black";
+  }
+});
 
-// prompt("choose a")
-// const module=Math.sqrt(2);
-// console.log(module);
-
-//first-solve
-
-// function sleepIn() {
-//   let firstanswer = "ish kuni emas";
-//   let secondanswer = "dam olish";
-//   let result = firstanswer || secondanswer;
-
-
-//   if (result) {
-//     alert("siz dama olyapsiz");
-//   } else {
-//     alert("siz ishdasiz");
-//   }
-// }
-
-// sleepIn(true,true);
-
-//second-solve
-
-function monkeyTrouble(a,b) {
+showForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
 
-    if (a&&b) {
-        alert("true");
-    }else if(!a && !b) {
-        alert("true");
+    if(!showForm.value){
+        showInput.style.borderBottom = "1px solid red";
+        showP.textContent = "Please enter your name";
+        showP.style.color = "red";
+        return;
     }
-    else {
-        alert("false");
-    }
-}
-
-monkeyTrouble(true,true)
-monkeyTrouble(false, false)
-monkeyTrouble(true, false)
-
-
-third-solve
-
-function parrotTrouble(parrot,time){
-   
-    if(parrot==true && 7<=time>=20){
-        alert("problem");
-    }else{
-        alert("no problem");
-    }
-}
-
-parrotTrouble(true,6);
-parrotTrouble(true,7);
-parrotTrouble(false, 6);
-
-//fourth-solve
-
-function sumDouble (a,b) {
-
-
-    if(a!=b){
-        alert(a+b)
-    }else{
-        alert((a+b)*2) 
-    }
-}
-
-// sumDouble (1,2)
-// sumDouble(3, 2)
-// sumDouble(2, 2)
-
-
-//fifth-solve
-
-function makes10 (a,b) {
-
-    if(a==10 || b==10){
-        alert("true");
-    }else if(a+b==10){
-        alert("true");
-    }else{
-        alert("false");
-    }
-
-}
-
-// makes10 (9,10)
-// makes10(9, 9)
-// makes10(1, 9)
-
-
-
-//sixth-solve
-
-function posNeg(a,b,negative) {
-
-    if(-a&&b){
-        alert("true");
-    }else if(a&&-b){
-      alert("true");
-    }else if(-a&&-b&&negative){
-       alert("true");
-    }else{
-        alert("false");
-    };    
-       
-
-    
-}
-
-
-posNeg(1, -1, false)
-// // posNeg(-1, 1, false)
-// posNeg(-4, -5, true)
-
-
-
-const sleepInterval =(a,b)
+});
