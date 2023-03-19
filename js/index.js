a96c54ad09ac8b40fddb1610923cefe886c7d4f0
lesson of 9 <komiljonovhumoyun982@gmail.com>
2023-03-19 19:59:52 +0500

    solves1

--- js/index.js
+++ js/index.js
@@ -1,114 +1,176 @@
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const list = document.getElementById('list');
-const modalaayer=document.querySelector(".modalaayer")
-const editedInput=document.querySelector("#editedInput")
-const editForm=document.querySelector("#editForm")
-// const img = document.createElement("img")
-// img.setAttribute("src" , "./img/items.svg")
+//exam
 
-let arr = [{
-  text:"Salom",
-  index:9999999,
-}];
-let index=0;
-let EditItem
-let result = "";
+//first solve
 
-  arr.forEach((item,index)=>{
-    result=result + `<li>  ${item.text} <div>
-    <img onclick="showModal(${item.id})" src="./img/circle.svg" alt="svg" />
-    <img onclick="removearr(${item.id})" src="./img/rubbishDash.svg" alt="svg" />
-    </div></li>`
-  });
-  console.log(result);
-  list.innerHTML =result;
+// function love6(num1, num2) {
+//   if (num1 == 6 || num2 == 6) {
+//     console.log("true");
+//   } else if (num1 + num2 == 6) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
+
+// love6(6, 4);
+// love6(4, 5);
+// love6(1, 5);
+
+// second solve
+
+// function cigarParty(num1,week){
+
+//     if((40<num1 && num1<60) || week==true ){
+//         console.log("true")
+//     }else{
+
+//         console.log("false")
+//     }
+// }
+
+// cigarParty(30, false)
+// cigarParty(50, false)
+// cigarParty(70, true)
+
+//third solve
+// function swap(str) {
+//   let result = "";
+//   for (let i = 0; i < str.length; i++) {
+//     if (
+//       str[i] === "a" ||
+//       str[i] === "e" ||
+//       str[i] === "i" ||
+//       str[i] === "o"||
+//       str[i] === "u"
+//     ) {
+//       let a = str[i].toUpperCase();
+//       result += a;
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(swap("Sunday"));
+// console.log(swap("Hello"));
+// console.log(swap("Codewars"));
+
+//fourth solve
+
+// function firstLast6(nums) {
+
+//     if ( (nums[0] == 6) || (nums[nums.length - 1]) == 6 ) {
+
+//       console.log("true")
+//     }else{
+
+//         console.log("false")
+//     }
+
+// }
+
+// firstLast6([6, 1, 2, 3])
+// firstLast6([13, 6, 1, 2, 3])
+// firstLast6([1, 2, 6])
+
+//fifth solve
+
+// function bobThere(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] == "b" && str[i+2]=="b") {
+//       return true;
+//     } else {
+
+//         return false;
+
+//     }
+//   }
+// }
+
+// console.log(bobThere('b9b'))
+// console.log(bobThere('bac'))
+// console.log(bobThere("abcbob"));
+
+// function solve(num){
+
+//   let result=num+2
+
+//   return function(){
+//     return result;
+//   }
+// }
+
+// let makesolve=solve(5)
+
+// console.log(makesolve)
 
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  arr.push({
-    text:input.value,
-    id:index
 
-  });
+// function again(){
 
-  index++
-  // console.log(arr);
-  let result = "";
-
-  arr.forEach((item,index)=>{
-    result=result + `<li>  ${item.text} <div>
-    <img onclick="showModal(${item.id})" src="./img/circle.svg" alt="svg" />
-    <img onclick="removearr(${item.id})" src="./img/rubbishDash.svg" alt="svg" />
-    </div></li>`
-  });
-  console.log(result);
-  list.innerHTML =result;
-})
+//   let num = prompt(
+//     "hohlagan soningizni yozin 0 dan katta 100 dan kichkina bolsin"
+//   );
+  
+//   if (num == 19 || num == 22 || num == 99) {
+//     alert("concratulation");
+//   } else {
+//     alert("try again");
+    
+//   }
+// }
 
 
-function removearr(id){
-  arr=arr.filter((item)=>item.id!==id);
+// again()
 
-  let result = "";
-  arr.forEach((item,index)=>{
-    result=result + `<li>  ${item.text} <div>
-    <img  onclick="showModal(${item.id})" src="./img/circle.svg" alt="svg" />
-    <img onclick="removearr(${item.id})" src="./img/rubbishDash.svg" alt="svg" />
-    </div></li>`
-  });
-  console.log(result);
-  list.innerHTML =result;
+// if (num == 19 || num == 22 || num == 99) {
+//   alert("concratulation");
+// } else {
+//   alert("try again");
+  
+// }
+
+
+for(let i=0 ; i<10 ; i++){
+  let num = prompt(
+    "hohlagan soningizni yozin 0 dan katta 100 dan kichkina bolsin"
+  );
+  
+  if (num == 19 || num == 22 || num == 99) {
+    alert("concratulation");
+  } else {
+    alert("try again");
+    
+  }
+
+
+  if(i==9){
+    i==0
+  }
 }
 
 
-function showModal(id){
-  modalaayer.style.display="flex";
-
-  let findEndObject=arr.find((item)=>item.id===id);
-  let findEndObjectIndex=arr.findIndex((item)=>item.id===id);
-  editedInput.value= findEndObject.text;
-  
-  EditItem={
-    index:findEndObjectIndex,
-    id:id, 
-  }
-};
-
-function editFormHandler(e){
-  e.preventDefault();
 
 
-  arr.splice(EditItem.index,1,{
-    text:editedInput.value,
-    id:EditItem.id,
-  });
-
-
-  let result = "";
-
-  arr.forEach((item,index)=>{
-    result=result + `<li>  ${item.text} <div>
-    <img onclick="showModal(${item.id})" src="./img/circle.svg" alt="svg" />
-    <img onclick="removearr(${item.id})" src="./img/rubbishDash.svg" alt="svg" />
-    </div></li>`
-  });
-  console.log(result);
-  list.innerHTML =result;
-
-  modalaayer.style.display="none";
-  
-};
+// let num = promt('hohlagan soningizni yozin 0 dan katta 100 dan kichik bolsin');
 
 
 
-editForm.addEventListener("submit",editFormHandler);
 
-modalaayer.addEventListener("click",(e)=>{
-   
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
 
-  if(e.target ==modalaayer)  
-  modalaayer.style.display="none";
-});
+// console.log(getRandomInt(100))
+
+
+// if(Math.floor(Math.random() * 100)==num){
+//   alert ("concratulation")
+// }else{
+//   alert "try again"
+// }
+
+
+
 
